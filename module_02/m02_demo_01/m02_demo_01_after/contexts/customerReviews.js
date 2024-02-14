8dd4ef794e06711ed0a31ab23073471021f85218
Rupesh Tiwari <roopkt@gmail.com>
2024-02-14 12:26:19 -0500

    chore: removed contexts.

--- module_02/m02_demo_01/m02_demo_01_after/contexts/customerReviews.js
+++
@@ -1,8 +0,0 @@
-let reviews = [];
-
-function addReview(bookId, review, rating) {
-  reviews.push({ bookId, review, rating });
-  return { message: 'Review added successfully', bookId };
-}
-
-module.exports = { addReview };
